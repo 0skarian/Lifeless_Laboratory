995635603d42c39d7289587c5b1ffae95a2f1ab8
0skarian <47034686+0skarian@users.noreply.github.com>
2019-12-10 16:50:15 +0100

    Created Primary [ShopData.lua] FIle

---
+++ ShopData/Master.lua
@@ -0,0 +1,45 @@
+local ShopData = {
+
+      {
+      
+        Name = "Angelic Presence",
+        Type = "Perk",
+        Description = "Nearby infected zombies will slowly turn back into their scientist counterpart.",
+        ItemCost = {"Bytes", 500},
+        Tags = {},
+        
+      },
+      
+       {
+      
+        Name = "Helping Hand",
+        Type = "Perk",
+        Description = "Everyone near you will slowly regenerate health - not zombies, though!",
+        ItemCost = {"Bytes", 200},
+        Tags = {},
+        
+      },
+      
+       {
+      
+        Name = "Common Sense",
+        Type = "Perk",
+        Description = "Gain a speed boost every time a zombie comes nearby, recharges every  2 minutes.",
+        ItemCost = {"Bytes", 400},
+        Tags = {},
+        
+      },
+      
+       {
+      
+        Name = "Rewind",
+        Type = "Ability",
+        Description = "It's rewind time! Go back to where you were 5 seconds ago - instantly.",
+        ItemCost = {"Bytes", 250},
+        Tags = {},
+        
+      }
+
+}
+
+return ShopData
